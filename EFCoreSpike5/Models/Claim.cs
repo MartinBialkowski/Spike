e088686667d9eacd9b8142c6703861c20a47d890
Martin Białkowski <embe2sc@gmail.com>
2017-08-19 19:49:36 +0200

    Changed model

--- EFCoreSpike5/Models/Claim.cs
+++ EFCoreSpike5/Models/Claim.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFCoreSpike5.Models
 {
     public class Claim
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Value { get; set; }
         public ICollection<UserClaim> UserClaims { get; set; }
     }
