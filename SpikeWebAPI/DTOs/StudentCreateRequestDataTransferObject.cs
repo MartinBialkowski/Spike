72584c37f22ca803196001caf5df0422a806be8f
Martin Białkowski <embe2sc@gmail.com>
2017-08-27 20:08:53 +0200

    created DTO sketch

---
+++ SpikeWebAPI/DTOs/StudentCreateRequestDataTransferObject.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreSpike5.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.DTOs
+{
+    public class StudentCreateRequestDataTransferObject: IValidatableObject
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public Course Course { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpikeWebAPI/DTOs/StudentUpdateRequestDataTransferObject.cs
@@ -0,0 +1,16 @@
+﻿using EFCoreSpike5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.DTOs
+{
+    public class StudentUpdateRequestDataTransferObject
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public Course Course { get; set; }
+    }
+}
