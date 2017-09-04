7041117dde54958a41ab02ba8e87cead95bc6a9e
Martin Białkowski <embe2sc@gmail.com>
2017-09-04 19:43:51 +0200

    Added tests for student update validator, bad path

--- SpikeWebAPI/DTOs/StudentUpdateRequestDataTransferObject.cs
+++ SpikeWebAPI/DTOs/StudentUpdateRequestDataTransferObject.cs
@@ -1,9 +1,17 @@
-﻿namespace SpikeWebAPI.DTOs
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpikeWebAPI.DTOs
 {
-    public class StudentUpdateRequestDataTransferObject
+    public class StudentUpdateRequestDataTransferObject: IValidatableObject
     {
         public int Id { get; set; }
         public int CourseId { get; set; }
         public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ SpikeWebAPI/Validators/StudentUpdateRequestDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SpikeWebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.Validators
+{
+    public class StudentUpdateRequestDTOValidator : AbstractValidator<StudentUpdateRequestDataTransferObject>
+    {
+        public StudentUpdateRequestDTOValidator()
+        {
+            RuleFor(student => student.Id).NotNull().WithMessage("Student Id cannot be null");
+            RuleFor(student => student.Id).GreaterThan(0).WithMessage("Student Id value must be greater than 0");
+            RuleFor(student => student.Name).NotEmpty().WithMessage("Student Id cannot be null or empty");
+            RuleFor(student => student.CourseId).NotNull().WithMessage("Course Id cannot be null");
+            RuleFor(student => student.CourseId).GreaterThan(0).WithMessage("Course Id value must be greater than 0");
+        }
+    }
+}
