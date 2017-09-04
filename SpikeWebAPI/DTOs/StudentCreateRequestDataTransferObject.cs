4281c015b99801265410672865c4b31cae4357ea
Martin Białkowski <embe2sc@gmail.com>
2017-09-04 19:17:32 +0200

    Validation for Student create request DTO

--- SpikeWebAPI/DTOs/StudentCreateRequestDataTransferObject.cs
+++ SpikeWebAPI/DTOs/StudentCreateRequestDataTransferObject.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using SpikeWebAPI.Validators;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace SpikeWebAPI.DTOs
 {
-    public class StudentCreateRequestDataTransferObject: IValidatableObject
+    public class StudentCreateRequestDataTransferObject : IValidatableObject
     {
         public int CourseId { get; set; }
         public string Name { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            throw new NotImplementedException();
+            var validator = new StudentCreateRequestDTOValidator();
+            var result = validator.Validate(this);
+            return result.Errors.Select(item => new ValidationResult(item.ErrorMessage, new[] { item.PropertyName }));
         }
     }
 }
