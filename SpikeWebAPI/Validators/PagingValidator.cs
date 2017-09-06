37c169c7abcf684b85229cda4e814408c89d970a
Martin Białkowski <embe2sc@gmail.com>
2017-09-06 22:34:35 +0200

    Added validation for page limit

---
+++ SpikeWebAPI/Validators/PagingValidator.cs
@@ -0,0 +1,18 @@
+﻿using EFCoreSpike5.ConstraintsModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.Validators
+{
+    public class PagingValidator : AbstractValidator<Paging>
+    {
+        public PagingValidator()
+        {
+            RuleFor(paging => paging.PageLimit).NotNull().WithMessage("page size cannot be null");
+            RuleFor(paging => paging.PageLimit).GreaterThan(0).WithMessage("page size must be greater than 0");
+        }
+    }
+}
---
+++ ValidatorsTest/PagingValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using EFCoreSpike5.ConstraintsModels;
+using SpikeWebAPI.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidatorsTest
+{
+    public class PagingValidatorTest
+    {
+        private int pageSize;
+        private int pageNumber;
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void InvalidWhenNameIsNotValid(int pageSize)
+        {
+            // arrange
+            this.pageSize = pageSize;
+            pageNumber = 1;
+            var validator = new PagingValidator();
+            // act
+            var result = validator.Validate(new Paging(pageNumber, pageSize));
+            // assert
+            Assert.False(result.IsValid);
+        }
+
+        
+
+        [Fact]
+        public void ValidWhenPageNumberAndPageSizeProvided()
+        {
+            // arrange
+            pageSize = 5;
+            pageNumber = 1;
+            var validator = new PagingValidator();
+            // act
+            var result = validator.Validate(new Paging(pageNumber, pageSize));
+            // assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
