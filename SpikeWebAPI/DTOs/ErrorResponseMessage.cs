16c921cad4bc6b1f448109fa58156a75ab59ef5f
Martin Białkowski <embe2sc@gmail.com>
2017-09-20 19:30:58 +0200

    Added class for errors, possible remove, need approval

---
+++ SpikeWebAPI/DTOs/ErrorResponseMessage.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.DTOs
+{
+    public class ErrorResponseMessage
+    {
+        public string UserMessage { get; set; }
+        public string InternalMessage { get; set; }
+        public string MoreInfo { get; set; }
+
+        public void CreateNotFoundError()
+        {
+            UserMessage = "Sorry, the requested resource does not exist";
+        }
+
+        public void CreateBadRequestError()
+        {
+            UserMessage = "Sorry, the given parameters are invalid";
+        }
+
+        public string GetErrorAsJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
