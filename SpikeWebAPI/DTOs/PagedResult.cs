c00c93bb009f4160ba5e689ed059b5d288e4c287
Martin Białkowski <embe2sc@gmail.com>
2017-09-20 21:12:49 +0200

    Added paged result

---
+++ SpikeWebAPI/DTOs/PagedResult.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace SpikeWebAPI.DTOs
+{
+    public class PagedResult<T>
+    {
+        /// <summary>
+        /// The page number this page represents. 
+        /// </summary>
+        public int PageNumber { get; set; }
+
+        /// <summary> 
+        /// The size of this page. 
+        /// </summary> 
+        public int PageSize { get; set; }
+
+        /// <summary> 
+        /// The total number of pages available. 
+        /// </summary> 
+        public int TotalNumberOfPages { get; set; }
+
+        /// <summary> 
+        /// The total number of records available. 
+        /// </summary> 
+        public int TotalNumberOfRecords { get; set; }
+
+        /// <summary> 
+        /// The URL to the first page
+        /// </summary> 
+        public string FirstPageUrl { get; set; }
+
+        /// <summary> 
+        /// The URL to the previous page
+        /// </summary> 
+        public string PreviousPageUrl { get; set; }
+
+        /// <summary> 
+        /// The URL to the next page 
+        /// </summary> 
+        public string NextPageUrl { get; set; }
+
+        /// <summary> 
+        /// The URL to the last page
+        /// </summary> 
+        public string LastPageUrl { get; set; }
+
+        /// <summary> 
+        /// The records this page represents. 
+        /// </summary> 
+        public IList<T> Results { get; set; }
+    }
+}
