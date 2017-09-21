9b933fe0a6582764dfedc3bc82d9843e1461aae2
Martin Białkowski <embe2sc@gmail.com>
2017-09-21 20:54:53 +0200

    Added DTOs for student and course response

--- SpikeWebAPI/DTOs/StudentResponseDataTransferObject.cs
+++ SpikeWebAPI/DTOs/StudentResponseDataTransferObject.cs
@@ -1,11 +1,9 @@
-﻿using EFCoreSpike5.Models;
-
-namespace SpikeWebAPI.DTOs
+﻿namespace SpikeWebAPI.DTOs
 {
     public class StudentResponseDataTransferObject
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Course Course { get; set; }
+        public CourseResponseDataTransferObject Course { get; set; }
     }
 }
