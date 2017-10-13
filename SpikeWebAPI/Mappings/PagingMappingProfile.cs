740aff43355503d925ccf985e99a8c5655add26a
Martin Białkowski <embe2sc@gmail.com>
2017-10-13 20:00:18 +0200

    Added mapper

---
+++ SpikeWebAPI/Mappings/PagingMappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using EFCoreSpike5.ConstraintsModels;
+using SpikeWebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpikeWebAPI.Mappings
+{
+    public class PagingMappingProfile : Profile
+    {
+        public PagingMappingProfile()
+        {
+            CreateMap(typeof(PagingDTO), typeof(Paging))
+                .ConvertUsing(typeof(PagingConverter));
+        }
+    }
+
+    public class PagingConverter : ITypeConverter<PagingDTO, Paging>
+    {
+        public Paging Convert(PagingDTO source, Paging destination, ResolutionContext context)
+        {
+            destination = new Paging
+            {
+                PageLimit = source.PageLimit.Value,
+                PageNumber = source.PageNumber.Value
+            };
+
+            return destination;
+        }
+    }
+}
