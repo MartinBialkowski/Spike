254d49aea1129c9c5907f8de9039d3adc79d1da5
Martin Białkowski <embe2sc@gmail.com>
2017-11-23 20:57:48 +0100

    Update DbContext

--- EFCoreSpike5/Models/EFCoreSpikeContext.cs
+++ EFCoreSpike5/Models/EFCoreSpikeContext.cs
@@ -1,29 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreSpike5.Models
 {
-    public class EFCoreSpikeContext : DbContext
+    public class EFCoreSpikeContext : IdentityDbContext
     {
-        public EFCoreSpikeContext(DbContextOptions<EFCoreSpikeContext> options): base(options)
+        public EFCoreSpikeContext(DbContextOptions<EFCoreSpikeContext> options) : base(options)
         {
         }
 
-        public DbSet<Claim> Claims { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserClaim> UserClaims { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Claim>().ToTable("Claim");
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Student>().ToTable("Student");
-            modelBuilder.Entity<User>().ToTable("User");
-            modelBuilder.Entity<UserClaim>().ToTable("UserClaim");
-
-            modelBuilder.Entity<UserClaim>()
-                .HasKey(c => new { c.ClaimId, c.UserId });
         }
     }
 }
