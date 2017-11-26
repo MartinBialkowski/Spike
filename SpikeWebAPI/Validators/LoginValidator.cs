66d10c85a349eaa6925aa5831b475c1ca3c02d80
Martin Białkowski <embe2sc@gmail.com>
2017-11-26 13:14:59 +0100

    Validator email not null or empty

---
+++ SpikeWebAPI/Validators/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SpikeWebAPI.DTOs;
+
+namespace SpikeWebAPI.Validators
+{
+    public class LoginValidator : AbstractValidator<UserDTO>
+    {
+        public LoginValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(login => login.Email)
+                .NotEmpty().WithMessage("Email address is required.");
+                
+        }
+    }
+}
---
+++ ValidatorsTest/LoginValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using SpikeWebAPI.DTOs;
+using SpikeWebAPI.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ValidatorsTest
+{
+    public class LoginValidatorTest
+    {
+        private string validEmail = "test@test.com";
+        private string validPassword = "Password123";
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public void InvalidWhenLoginEmailNotProvided(string email)
+        {
+
+            // arrange
+            var validator = new LoginValidator();
+            var login = new UserDTO()
+            {
+                Email = email,
+                Password = validPassword
+            };
+            // act
+            var result = validator.Validate(login);
+            // assert
+            Assert.False(result.IsValid);
+        }
+    }
+}
