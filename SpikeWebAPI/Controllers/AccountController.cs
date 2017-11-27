e3189ef473de28e9451d8d33e95651bb762ff1ac
Martin Białkowski <embe2sc@gmail.com>
2017-11-27 22:05:35 +0100

    Partialy created Login

---
+++ SpikeWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpikeWebAPI.DTOs;
+
+namespace SpikeWebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserDTO userDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var appUser = userManager.Users.SingleOrDefault(r => r.Email == userDTO.Email);
+                //return await GenerateJwtToken(model.Email, appUser);
+            }
+
+            return Unauthorized();
+        }
+    }
+}
