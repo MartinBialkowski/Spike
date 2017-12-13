f23140beff41cbadb992702d6175f51ba793890e
Martin Białkowski <embe2sc@gmail.com>
2017-12-13 21:29:56 +0100

    Added extension to send confirmation email

---
+++ SpikeConnectProviders/Extensions/EmailSenderExtension.cs
@@ -0,0 +1,19 @@
+﻿using SpikeConnectProviders.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace SpikeConnectProviders.Extensions
+{
+    public static class EmailSenderExtension
+    {
+        public static Task SendConfirmationEmail(this IEmailSender emailSender, string email, string link)
+        {
+            string subject = "Confirm your email";
+            string message = $"Click this <a href='{HtmlEncoder.Default.Encode(link)}'>link</a> to confirm your email";
+            return emailSender.SendEmailAsync(email, subject, message);
+        }
+    }
+}
