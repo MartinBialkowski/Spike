1396234133eacea54d0aedd6ddeb204d1529005a
Martin Białkowski <embe2sc@gmail.com>
2017-12-13 21:29:17 +0100

    Implemented SendGrid (email sender)

---
+++ SpikeConnectProviders/Abstract/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace SpikeConnectProviders.Abstract
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string receiverEmail, string subject, string message);
+
+        Task Execute(string apiKey, string receiverEmail, string subject, string message);
+    }
+}
---
+++ SpikeConnectProviders/Services/SendGridEmailSender.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SpikeConnectProviders.Abstract;
+using EFCoreSpike5.CommonModels;
+using Microsoft.Extensions.Options;
+
+namespace SpikeConnectProviders
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        public SendGridEmailSender(IOptions<SendGridOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public SendGridOptions Options { get; set; }
+
+        public Task SendEmailAsync(string receiverEmail, string subject, string message)
+        {
+            return Execute(Options.ApiKey, receiverEmail, subject, message);
+        }
+
+        public Task Execute(string apiKey, string receiverEmail, string subject, string message)
+        {
+            var client = new SendGridClient(apiKey);
+            var sendGridMessage = new SendGridMessage()
+            {
+                From = new EmailAddress(Options.SenderEmail, Options.SenderName),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            sendGridMessage.AddTo(receiverEmail);
+            return client.SendEmailAsync(sendGridMessage);
+        }
+    }
+}
