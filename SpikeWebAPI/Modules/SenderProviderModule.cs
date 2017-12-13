daa27576018724924d2fed2baa390c9eb42b1808
Martin Białkowski <embe2sc@gmail.com>
2017-12-13 21:55:33 +0100

    Added module for EmailSender + API configuration update

---
+++ SpikeWebAPI/Modules/SenderProviderModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using SpikeConnectProviders;
+using SpikeConnectProviders.Abstract;
+
+namespace SpikeWebAPI.Modules
+{
+    public class SenderProviderModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SendGridEmailSender>().As<IEmailSender>();
+        }
+    }
+}
