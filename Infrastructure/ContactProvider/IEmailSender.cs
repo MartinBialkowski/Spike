45ea3d809571d0cafe71159a21a18c5a07128e7d
Martin Białkowski <embe2sc@gmail.com>
2017-12-19 23:09:35 +0100

    Added new method for email sender

--- Infrastructure/ContactProvider/IEmailSender.cs
+++ Infrastructure/ContactProvider/IEmailSender.cs
@@ -7,5 +7,6 @@ namespace Infrastructure.ContactProvider
         Task SendEmailAsync(string receiverEmail, string subject, string message);
         Task Execute(string apiKey, string receiverEmail, string subject, string message);
         Task SendConfirmationEmail(string email, string link);
+        Task SendResetPasswordEmail(string email, string link);
     }
 }
