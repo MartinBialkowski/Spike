ea3f246818cbb3538718631c2d26e0e41f8179f2
Martin Białkowski <embe2sc@gmail.com>
2017-12-19 23:10:19 +0100

    Added validators

---
+++ ValidatorsTest/ForgotPasswordValidatorTest.cs
@@ -0,0 +1,45 @@
+﻿using SpikeWebAPI.DTOs;
+using SpikeWebAPI.Validators;
+using Xunit;
+
+namespace ValidatorsTest
+{
+    public class ForgotPasswordValidatorTest
+    {
+        private string validEmail = "test@test.com";
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData("InvalidEmailAtProviderDotCom")]
+        public void InvalidWhenEmailNotProvided(string email)
+        {
+            // arrange
+            var validator = new ForgotPasswordValidator();
+            var dto = new ForgotPasswordDTO()
+            {
+                Email = email
+            };
+            // act
+            var result = validator.Validate(dto);
+            // assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void ValidWhenEmailAndPasswordProvided()
+        {
+            // arrange
+            var validator = new ForgotPasswordValidator();
+            var login = new ForgotPasswordDTO()
+            {
+                Email = validEmail
+            };
+            // act
+            var result = validator.Validate(login);
+            // assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
