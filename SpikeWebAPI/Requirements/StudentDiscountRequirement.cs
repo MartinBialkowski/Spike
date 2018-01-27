30d1af73a87711413e29c9654e10527f1f829eea
Martin Białkowski <embe2sc@gmail.com>
2018-01-27 13:56:41 +0100

    Added requirement

---
+++ SpikeWebAPI/Requirements/StudentDiscountRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Spike.WebApi.Requirements
+{
+    public class StudentDiscountRequirement : IAuthorizationRequirement
+    {
+        public int MaxAge { get; private set; }
+
+        public StudentDiscountRequirement(int maxAge = 26)
+        {
+            MaxAge = maxAge;
+        }
+    }
+}
