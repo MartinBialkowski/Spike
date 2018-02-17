0544ef2b716b1f5acc73596b4eeb297570e1c4ea
Martin Białkowski <embe2sc@gmail.com>
2018-02-17 18:10:45 +0100

    Added fixture for tests

---
+++ Spike.AuthenticationServer.IdentityServer.IntegrationTest/AuthenticationFixture.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+
+namespace Spike.AuthenticationServer.IdentityServer.IntegrationTest
+{
+    public class AuthenticationFixture : IDisposable
+    {
+        public readonly TestServer server;
+
+        public AuthenticationFixture()
+        {
+            server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+        }
+
+        public void Dispose()
+        {
+            server.Dispose();
+        }
+    }
+}
