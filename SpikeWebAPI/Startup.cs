974063bbb274bb8ec87a6708d4bead7357cba813
Martin Białkowski <embe2sc@gmail.com>
2018-03-31 21:37:46 +0200

    Fully updated to use net core 2.1

--- SpikeWebAPI/Startup.cs
+++ SpikeWebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -78,6 +79,7 @@ namespace Spike.WebApi
 
             services
                 .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddFluentValidation()
                 .AddJsonOptions(options =>
                 {
